57b11942de92b75f304b64c394d59bcaf51ed154
Yunze Li <yunze.li@careem.com>
2021-06-26 12:11:16 +0400

    add custom marker and custom info windows

--- app/src/main/java/com/arctos/mochabear/mapdemo/GoogleMapDemoActivity.kt
+++ app/src/main/java/com/arctos/mochabear/mapdemo/GoogleMapDemoActivity.kt
@@ -2,6 +2,7 @@ package com.arctos.mochabear.mapdemo
 
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
 import androidx.constraintlayout.widget.ConstraintLayout
@@ -10,9 +11,12 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.maps.android.ui.IconGenerator
 
 class GoogleMapDemoActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -30,15 +34,15 @@ class GoogleMapDemoActivity : AppCompatActivity(), OnMapReadyCallback {
 
         demoButton1.setOnClickListener {
             val beijingLatLng = LatLng(39.916668, 116.383331)
-            addMarkedAndMoveCamera(beijingLatLng, "This is beijing")
+            addMarkedAndMoveCamera(beijingLatLng, "This is beijing", "The Capital of China.")
         }
         demoButton2.setOnClickListener {
             val seattleLatLng = LatLng(47.608013, -122.335167)
-            addMarkedAndMoveCamera(seattleLatLng, "This is Seattle")
+            addMarkedAndMoveCamera(seattleLatLng, "This is Seattle", "Where Amazon headquarter is.")
         }
         demoButton3.setOnClickListener {
             val dubaiLatLng = LatLng(25.276987, 55.296249)
-            addMarkedAndMoveCamera(dubaiLatLng, "This is Dubai")
+            addMarkedAndMoveCamera(dubaiLatLng, "This is Dubai", "Currently I stay in here.")
         }
 
 
@@ -62,16 +66,41 @@ class GoogleMapDemoActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
-        val sydney = LatLng(-33.852, 151.211)
         this.googleMap = googleMap
     }
 
-    private fun addMarkedAndMoveCamera(destination: LatLng, title: String) {
+    private fun addMarkedAndMoveCamera(destination: LatLng, title: String, description: String) {
+        val customMarkerView = bindCustomMarkerInfo(title, description)
+        val iconGenerator = IconGenerator(this)
+        iconGenerator.setContentView(customMarkerView)
+        iconGenerator.setBackground(null)
+        val iconBitMap = iconGenerator.makeIcon()
+
         googleMap.addMarker(
             MarkerOptions()
                 .position(destination)
-                .title(title)
+                .icon(BitmapDescriptorFactory.fromBitmap(iconBitMap))
         )
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(destination))
+        googleMap.setInfoWindowAdapter(object : GoogleMap.InfoWindowAdapter {
+            override fun getInfoWindow(p0: Marker): View? {
+                // in here it will using the entire info window including frame
+                return null
+            }
+
+            override fun getInfoContents(p0: Marker): View? {
+                // in here it will show custom content but using default info window frame and background
+                return null
+            }
+
+        })
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(destination, 12.0f))
+    }
+
+    private fun bindCustomMarkerInfo(title: String, description: String): View {
+        val customMarkerView = layoutInflater.inflate(R.layout.google_map_custom_marker, null)
+        customMarkerView.findViewById<TextView>(R.id.custom_marker_title)?.apply { text = title }
+        customMarkerView.findViewById<TextView>(R.id.customer_marker_description)
+            ?.apply { text = description }
+        return customMarkerView
     }
 }
--- leetcode/README.md
+++ leetcode/README.md
@@ -1,17 +1,31 @@
+# LeetCode刷题记录
+
+## Medium
+
+[307.Range Sum Query - Mutable](https://leetcode.com/problems/range-sum-query-mutable/) Binary
+Indexed Tree (Fenwick Tree)
+
+## Easy
+
+## Hard
+
+[629. K Inverse Pairs Array](https://leetcode.com/problems/k-inverse-pairs-array/) dp[i][j] =
+dp[i-1][j] + dp[i-1][j-1] +... +dp [i-1][j-i+1]
+[778. Swim in Rising Water](https://leetcode.com/problems/swim-in-rising-water/description/) BFS +
+Priority Queue
+
 # Java/Kotlin Code Snippet
 
-## Collection Fast Initialization
-
-### Arrays
+### Initialize arrays
 
 ```java
-    // initialized array with fix length (fill with default value)
-    int[][]graph=new int[3][3];
+// initialized array with fix length (fill with default value)
+int[][]graph=new int[3][3];
 
-            // initialized array with content
-            int[][]graph={{1,2},{2,3},{3,4},{1,4},{1,5}};
+// initialized array with content
+int[][]graph={{1,2},{2,3},{3,4},{1,4},{1,5}};
 ```
 
-### ArrayList
+### Initialize arrayList
 
 
