1aa36d6c0d027d7d6368d8aacd4fb48d6e1d3487
Yunze Li <liyunze0611@163.com>
2022-03-16 12:13:51 +0800

    add lifecycle logs

--- app/src/main/java/com/arctos/mochabear/main/MainActivity.kt
+++ app/src/main/java/com/arctos/mochabear/main/MainActivity.kt
@@ -2,12 +2,15 @@ package com.arctos.mochabear.main
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
 import com.arctos.mochabear.R
 import com.arctos.mochabear.mapdemo.GoogleMapDemoActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = this.javaClass.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +22,38 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, GoogleMapDemoActivity::class.java)
             startActivity(intent)
         }
+
+        Log.d(TAG, "onCreate is triggered!")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart is triggered!")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume is triggered!")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause is triggered!")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop is triggered!")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy is triggered!")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart is triggered!")
     }
 
 }
--- leetcode/src/main/kotlin/com/arctos/mochabear/leetcode/KotlinSolution.kt
+++ leetcode/src/main/kotlin/com/arctos/mochabear/leetcode/KotlinSolution.kt
@@ -1,4 +1,28 @@
 package com.arctos.mochabear.leetcode
 
-class KotlinSolution {
+import java.util.Stack
+
+class Solution {
+    fun validateStackSequences(pushed: IntArray, popped: IntArray): Boolean {
+        if (pushed.isEmpty() && popped.isEmpty()) {
+            return true
+        } else if (pushed.isEmpty() || popped.isEmpty() || pushed.size != popped.size) {
+            return false
+        }
+
+        val stack = Stack<Int>()
+        var poppedIndex = 0
+        for (pushedIndex in pushed.indices) {
+            stack.push(pushed[pushedIndex])
+            while (stack.isNotEmpty() && stack.peek() == popped[poppedIndex]) {
+                stack.pop()
+                poppedIndex++
+            }
+        }
+        return stack.isEmpty()
+    }
+}
+
+fun main(args: Array<String>) {
+    val solution = Solution()
 }
