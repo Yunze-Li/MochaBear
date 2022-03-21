71cdd1ae13109608005c714fd58d56e0194b8df0
Yunze Li <liyunze0611@163.com>
2022-03-21 22:02:43 +0800

    add custom view demo

---
+++ app/src/main/java/com/arctos/mochabear/customviewdemo/CustomViewDemoActivity.kt
@@ -0,0 +1,13 @@
+package com.arctos.mochabear.customviewdemo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CustomViewDemoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
--- app/src/main/java/com/arctos/mochabear/main/MainActivity.kt
+++ app/src/main/java/com/arctos/mochabear/main/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.arctos.mochabear.main
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
 import com.arctos.mochabear.R
-import com.arctos.mochabear.mapdemo.GoogleMapDemoActivity
+import com.arctos.mochabear.customviewdemo.CustomViewDemoActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,16 +20,30 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate is triggered!")
 
         setContentView(R.layout.main_activity)
-        val googleMapDemoButton = findViewById<AppCompatButton>(R.id.google_map_demo_button)
 
+        // Google Map Demo
+        val googleMapDemoButton = findViewById<AppCompatButton>(R.id.google_map_demo_button)
         googleMapDemoButton.setOnClickListener {
-            val intent = Intent(this, GoogleMapDemoActivity::class.java)
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse("https://www.baidu.com")
             startActivity(intent)
         }
 
-        Log.d(TAG, "onCreate is triggered!")
+        // Custom View Demo
+        val customViewDemoButton = findViewById<AppCompatButton>(R.id.custom_view_demo_button)
+        customViewDemoButton.setOnClickListener {
+            val intent = Intent(this, CustomViewDemoActivity::class.java)
+            val resultLauncher = registerForActivityResult(StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    val data: Intent? = result.data
+                    Log.d(TAG, "data: $data received!")
+                }
+            }
+            resultLauncher.launch(intent)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -36,9 +53,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.add_item -> Toast.makeText(this, "You clicked Add", Toast.LENGTH_SHORT).show()
-            R.id.remove_item -> Toast.makeText(this, "You clicked Remove", Toast.LENGTH_SHORT)
-                .show()
+            R.id.option_1 -> Toast.makeText(this, "You clicked Option 1", Toast.LENGTH_SHORT).show()
+            R.id.option_2 -> Toast.makeText(this, "You clicked Option 2", Toast.LENGTH_SHORT).show()
         }
         return true
     }
