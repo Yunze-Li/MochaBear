1b62dc8ed4913d96f8961101037045c3f4331dcf
Yunze Li <liyunze0611@163.com>
2022-03-30 09:45:01 +0800

    add three unsolved problems

---
+++ leetcode/src/main/kotlin/com/arctos/mochabear/leetcode/medium/UnSolved300.kt
@@ -0,0 +1,10 @@
+package com.arctos.mochabear.leetcode.medium
+
+/**
+ * https://leetcode.com/problems/longest-increasing-subsequence/
+ */
+class UnSolved300 {
+    fun lengthOfLIS(nums: IntArray): Int {
+        return 0
+    }
+}
---
+++ leetcode/src/main/kotlin/com/arctos/mochabear/leetcode/medium/UnSovled236.kt
@@ -0,0 +1,12 @@
+package com.arctos.mochabear.leetcode.medium
+
+import com.arctos.mochabear.leetcode.model.TreeNode
+
+/**
+ * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+ */
+class UnSovled236 {
+    fun lowestCommonAncestor(root: TreeNode?, p: TreeNode?, q: TreeNode?): TreeNode? {
+        return null
+    }
+}
