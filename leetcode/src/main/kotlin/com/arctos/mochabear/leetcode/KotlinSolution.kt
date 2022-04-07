74b039b3498d2ba843aaee5236460ef21a3b6c45
Yunze Li <liyunze0611@163.com>
2022-04-07 10:36:34 +0800

    clean up KotlinSolution and JavaSolution

--- leetcode/src/main/kotlin/com/arctos/mochabear/leetcode/KotlinSolution.kt
+++ leetcode/src/main/kotlin/com/arctos/mochabear/leetcode/KotlinSolution.kt
@@ -1,29 +1,7 @@
 package com.arctos.mochabear.leetcode
 
-import kotlin.math.min
-
-
 class Solution {
-    fun minPathSum(grid: Array<IntArray>): Int {
-        val row = grid.size
-        val column = grid[0].size
-
-        for (rowIndex in 1 until row) {
-            grid[rowIndex][0] += grid[rowIndex - 1][0]
-        }
-        for (columnIndex in 1 until row) {
-            grid[0][columnIndex] += grid[0][columnIndex - 1]
-        }
-        for (rowIndex in 1 until row) {
-            for (columnIndex in 1 until column) {
-                grid[rowIndex][columnIndex] += min(
-                    grid[rowIndex - 1][columnIndex],
-                    grid[rowIndex][columnIndex - 1]
-                )
-            }
-        }
-        return grid[row - 1][column - 1]
-    }
+    // write code in here
 }
 
 fun main(args: Array<String>) {
