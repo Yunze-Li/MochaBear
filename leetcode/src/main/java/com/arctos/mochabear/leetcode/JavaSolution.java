acec4e7dad7c3f7181cbf89f741bed0d7f72884e
Yunze Li <liyunze0611@163.com>
2022-04-12 17:22:49 +0800

    clean up and add Unsolved4

--- leetcode/src/main/java/com/arctos/mochabear/leetcode/JavaSolution.java
+++ leetcode/src/main/java/com/arctos/mochabear/leetcode/JavaSolution.java
@@ -2,4 +2,9 @@ package com.arctos.mochabear.leetcode;
 
 public class JavaSolution {
     // write code here
+
+
+    public static void main(String[] args) {
+
+    }
 }
---
+++ leetcode/src/main/kotlin/com/arctos/mochabear/leetcode/hard/Unsolved4.kt
@@ -0,0 +1,8 @@
+package com.arctos.mochabear.leetcode.hard
+
+/**
+ * https://leetcode.com/problems/median-of-two-sorted-arrays/
+ */
+
+class Unsolved4 {
+}
