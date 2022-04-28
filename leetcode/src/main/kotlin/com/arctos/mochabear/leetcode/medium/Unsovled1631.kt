1132bcdd9146208e9b3abdb761cdf63a519217f9
Yunze Li <liyunze0611@163.com>
2022-04-28 08:27:48 +0800

    add unsolved problem 1631

---
+++ leetcode/src/main/kotlin/com/arctos/mochabear/leetcode/medium/Unsovled1631.kt
@@ -0,0 +1,11 @@
+package com.arctos.mochabear.leetcode.medium
+
+/**
+ * https://leetcode.com/problems/path-with-minimum-effort/
+ */
+
+class Unsovled1631 {
+    fun minimumEffortPath(heights: Array<IntArray>): Int {
+        return 0
+    }
+}
